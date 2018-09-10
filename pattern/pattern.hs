c5f1cf65ba653530279e43dbdaec8db63250b0bf
ivanwarke <iveon.cc@gmail.com>
2018-09-11 01:38:37 +0800

    That's the beauty of functional programming

--- pattern/pattern.hs
+++ pattern/pattern.hs
@@ -48,3 +48,22 @@ length' (_:xs) = 1 + length' xs
 sum' :: (Num a) => [a] -> a
 sum' [] = 0
 sum' (x:xs) = x + sum' xs
+
+-- The syntax for case expressions is pretty simple:
+-- case <expression> of <pattern> -> result
+--                      <pattern> -> result
+--                      <pattern> -> result
+--                      ...
+head'' :: [a] -> a
+head'' xs = case xs of [] -> error "No head for empty lists!"
+                       (x:_) -> x
+
+describeList :: [a] -> String
+describeList xs = "The list is " ++ case xs of []  -> "emplty."
+                                               [x] -> "a singleton list."
+                                               xs  -> "a longer list."
+
+describeList' xs = "The list is" ++ what xs
+  where what []  = "empty."
+        what [x] = "a singleton list."
+        what xs  = "a longer list."
