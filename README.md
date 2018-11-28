4231578e21153c0889cfa9a7f87df6820dc19e2a
ivanwarke <iveon.cc@gmail.com>
2018-11-28 20:44:58 +0800

    delete all sh*t files

--- README.md
+++
@@ -1,3 +0,0 @@
-# Haskelll
-
-Haskell is cool :)
--- demo/helloworld.hs
+++
@@ -1,5 +0,0 @@
-main = do
-    putStrLn "hello, world"
-    putStrLn "hey, what's your name?"
-    name <- getLine
-    putStrLn ("hey " ++ name ++ ", you rock!")
